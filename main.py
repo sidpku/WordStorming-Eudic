00563ca61ebfeaa18d873ac4571572be2d53b1fa
sidpku <pkusid@gmail.com>
2019-05-25 23:09:07 +0800

    adding main,py and output_file

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 # 主程序
-# 只是拿来测试下GIT好不好用，今天先不写程序了。
+FILENAME = 'raw_material.txt'
+
+with open(FILENAME, encoding='UTF-8') as source_file:
+    content = source_file.readlines()
+
+# 开关，如果1则开始录入单词。
+switch = 0
+output = ''
+
+for line in content:
+    for i in line:
+        if i == '@' or i == '@':
+            switch = 1 - switch
+        elif switch == 1:
+            output = output + i
+        else:
+            pass
+    if output[-1] == ';':
+        continue
+    output = output + ';'
+
+# print(output)
+with open('output.txt',mode='w') as output_file:
+    output_file.write(output)
+
